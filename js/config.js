cb2d514080cdeaea3ef1df350665404cc822afb0
TheOneAndOnly <209theoneandonly@gmail.com>
2024-10-12 17:19:39 -0700

    Change to 12-hr clock

--- js/config.js
+++ js/config.js
@@ -295,13 +295,12 @@ const CONFIG = {
   /**
    * The delimiter between the hours and minutes on the clock.
    */
-  clockDelimiter: ' ',
+  clockDelimiter: '.',
 
   /**
    * Show a twenty-four-hour clock instead of a twelve-hour clock with AM/PM.
    */
-  twentyFourHourClock: true,
-
+  twentyFourHourClock: false,
   /**
    * File extension for icon images
    */
